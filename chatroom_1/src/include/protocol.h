438d0c3c5401a107a55ba9d0f47a394485381d41
linnaid <jiaguoyangyang@qq.com>
2025-07-23 21:43:59 +0800

    proto

---
+++ chatroom_1/src/include/protocol.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "include.h"
+#include "user_page.h"
+#include "user_chat.pb.h"
+
+class Protocol {
+public:
+    static std::string pack(const std::string& payload);
+    
+    static bool unpack(std::string& buffer, chat::Chat& chat_msg);
+
+    static bool is_heartbeat(const std::string& msg);
+
+    static std::string GetNowTime();
+
+    static void Parse_msg(const std::string& msg, chat::Chat& chat_msg);
+};
