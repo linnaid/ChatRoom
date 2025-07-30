b7e4eae3b0ac1de32f86d1b641406fcda61592c9
linnaid <jiaguoyangyang@qq.com>
2025-07-30 09:36:48 +0800

    hello

--- 2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-class UserServer {
-public:
-    std::string GenerateVerifiCode();
-    void SendCodeToEmail(const std::string &code, const std::string &email);
-};
